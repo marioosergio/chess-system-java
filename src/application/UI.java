3a09a6bda5a1b47cadf01157fbfd2af66bec758d
Mario Sergio <msergioc@gmail.com>
2022-09-08 19:27:07 -0300

    Implementing possible moves of Rook

--- src/application/UI.java
+++ src/application/UI.java
@@ -43,7 +43,7 @@ public class UI {
             int row = Integer.parseInt(s.substring(1));
             return new ChessPosition(column, row);
         } catch (RuntimeException e) {
-            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
+            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
         }
     }
 
